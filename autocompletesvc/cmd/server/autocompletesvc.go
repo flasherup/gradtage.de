2cdd7dab549ffaada1cb238e16f87a3fe3555415
Valeriy Vostrikov <>
2020-04-16 20:39:36 +0300

    Add autocomplete main file

---
+++ autocompletesvc/cmd/server/autocompletesvc.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"github.com/flasherup/gradtage.de/alertsvc"
+	"github.com/flasherup/gradtage.de/autocompletesvc"
+	"github.com/flasherup/gradtage.de/autocompletesvc/acrpc"
+	"github.com/flasherup/gradtage.de/autocompletesvc/config"
+	"github.com/flasherup/gradtage.de/autocompletesvc/impl"
+	"github.com/flasherup/gradtage.de/autocompletesvc/impl/database"
+	"github.com/flasherup/gradtage.de/common"
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	alert "github.com/flasherup/gradtage.de/alertsvc/impl"
+	googlerpc "google.golang.org/grpc"
+)
+
+func main() {
+	configFile := flag.String("config.file", "src/config.yml", "Config file name.")
+	flag.Parse()
+
+	var logger log.Logger
+	{
+		logger = log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
+		logger = level.NewFilter(logger, level.AllowDebug())
+		logger = log.With(logger,
+			"svc", "stationssvc",
+			"ts", log.DefaultTimestampUTC,
+			"caller", log.Caller(3),
+		)
+	}
+
+	//Config
+	conf, err := config.LoadConfig(*configFile)
+	if err != nil {
+		level.Error(logger).Log("msg", "config loading error", "exit", err.Error())
+		return
+	}
+
+	//Database
+	db, err := database.NewPostgres(conf.Database)
+	if err != nil {
+		level.Error(logger).Log("msg", "database error", "exit", err.Error())
+		return
+	}
+	defer db.Dispose()
+
+
+	var alertService alertsvc.Client
+	if conf.AlertsEnable {
+		alertService = alert.NewAlertSCVClient(conf.Clients.AlertAddr, logger)
+	} else {
+		alertService = common.NewSilentAlert()
+	}
+
+
+
+	level.Info(logger).Log("msg", "service started")
+	defer level.Info(logger).Log("msg", "service ended")
+
+	alertService.SendAlert(impl.NewNotificationAlert("service started"))
+
+	ctx := context.Background()
+	autocompleteService, err := impl.NewAutocompleteSVC(logger, db, alertService)
+	if err != nil {
+		level.Error(logger).Log("msg", "service error", "exit", err.Error())
+		return
+	}
+	errors := make(chan error)
+
+	go func() {
+		listener, err := net.Listen("tcp", conf.GetGRPCAddress())
+		if err != nil {
+			errors <- err
+			return
+		}
+
+		gRPCServer := googlerpc.NewServer()
+		acrpc.RegisterAutocompleteSVCServer(gRPCServer, autocompletesvc.NewGRPCServer(ctx, autocompletesvc.Endpoints {
+			GetAutocompleteEndpoint:    autocompletesvc.MakeGetAutocompleteEndpoint(autocompleteService),
+			AddSourcesEndpoint:    autocompletesvc.MakeAddSourcesEndpoint(autocompleteService),
+		}))
+
+		level.Info(logger).Log("transport", "GRPC", "addr", conf.GetGRPCAddress())
+		errors <- gRPCServer.Serve(listener)
+	}()
+
+	//fmt.Println(<-errors)
+
+	metrics := autocompletesvc.NewMetricsTransport(autocompleteService,logger)
+
+	go func() {
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		errors <- fmt.Errorf("%s", <-c)
+	}()
+
+	go func() {
+		level.Info(logger).Log("transport", "HTTP", "addr", conf.GetHTTPAddress())
+		server := &http.Server{
+			Addr:    conf.GetHTTPAddress(),
+			Handler: metrics,
+		}
+		errors <- server.ListenAndServe()
+	}()
+
+	level.Error(logger).Log("exit", <-errors)
+	alertService.SendAlert(impl.NewNotificationAlert("service stopped"))
+}
