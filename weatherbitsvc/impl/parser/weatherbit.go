c7da451056ff51f2ed77d15878904f3023e051b5
qq <antoha.grom228@gmail.com>
2021-01-27 19:49:23 +0200

    Finished the WeatherBit parser

--- weatherbitsvc/impl/parser/weatherbit.go
+++ weatherbitsvc/impl/parser/weatherbit.go
@@ -5,9 +5,67 @@ import (
 	"fmt"
 )
 
-type WeatherBitData struct {
-	Count int `json:"count"`
+
+
+type Weather struct {
+	Icon        string `json:"icon"`
+	Code        int    `json:"code"`
+	Description string `json:"description"`
 }
+
+type Data struct {
+	Rh           int     `json:"rh"`
+	Pod          string  `json:"pod"`
+	Lon          float64 `json:"lon"`
+	Pres         float64 `json:"pres"`
+	Timezone     string  `json:"timezone"`
+	ObTime       string  `json:"ob_time"`
+	CountryCode  string  `json:"country_code"`
+	Clouds       float64 `json:"clouds"`
+	TS           int     `json:"ts"`
+	SolarRad     float64 `json:"solar_rad"`
+	StateCode    string  `json:"state_code"`
+	CityName     string  `json:"city_name"`
+	WindSPD      float64 `json:"wind_spd"`
+	WindCdirFull string  `json:"wind_cdir_full"`
+	WindCdir     string  `json:"wind_cdir"`
+	SLP          float64 `json:"slp"`
+	Vis          float64 `json:"vis"`
+	HAngle       float64 `json:"h_angle"`
+	Sunset       string  `json:"sunset"`
+	DNI          float64 `json:"dni"`
+	Dewpt        float64 `json:"dewpt"`
+	Snow         int     `json:"snow"`
+	UV           float64 `json:"uv"`
+	Precip       float64 `json:"precip"`
+	WindDir      float64 `json:"wind_dir"`
+	Sunrise      string  `json:"sunrise"`
+	GHI          float64 `json:"ghi"`
+	DHI          float64 `json:"dhi"`
+	AQI          float64 `json:"aqi"`
+	Lat          float64 `json:"lat"`
+	Weather      Weather `json:"weather"`
+	DateTime     string  `json:"date_time"`
+	Temp         float64 `json:"temp"`
+	Station      string  `json:"station"`
+	ElevAngle    float64 `json:"elev_angle"`
+	AppTemp      float64 `json:"app_temp"`
+}
+
+type Minutely struct {
+	TimestampUTC   string  `json:"timestamp_utc"`
+	Snow           int     `json:"snow"`
+	TimestampLocal string  `json:"timestamp_local"`
+	TS             int     `json:"ts"`
+	Precip         float64 `json:"precip"`
+}
+
+type WeatherBitData struct {
+	Count    int        `json:"count"`
+	Data     []Data     `json:"data"`
+	Minutely []Minutely `json:"minutely"`
+}
+
 
 func ParseWeatherBit(data *[]byte) (*WeatherBitData, error) {
 	fmt.Println(string(*data))
