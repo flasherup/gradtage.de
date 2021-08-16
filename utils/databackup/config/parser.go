514d3fe802c0df94de598e0f38e52c9011148756
qq <antoha.grom228@gmail.com>
2021-08-16 11:14:17 +0300

    not ready

---
+++ utils/databackup/config/parser.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+)
+
+type Clients struct {
+	SRCAddr 		string `yaml:"src_addr"`
+	RCVRAddr 		string `yaml:"rcvr_addr"`
+}
+
+type DatabaseConfig struct {
+	Name		string	`yaml:"name"`
+	Host 		string 	`yaml:"host"`
+	Port 		int 	`yaml:"port"`
+	User 		string 	`yaml:"user"`
+	Password 	string 	`yaml:"password"`
+}
+
+type WeatherBitConfig struct {
+	Clients		Clients 		`yaml:"clients"`
+	Database 	DatabaseConfig	`yaml:"database"`
+}
+
+func LoadConfig(path string) (config *WeatherBitConfig, err error) {
+	c, err := ioutil.ReadFile(path)
+	if err != nil {
+		return
+	}
+
+	config = &WeatherBitConfig{}
+
+	err = yaml.Unmarshal(c, config)
+	if err != nil {
+		return
+	}
+
+	return
+}
