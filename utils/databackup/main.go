18696e7686bb515549c6996993d7675c733b28db
qq <antoha.grom228@gmail.com>
2021-09-06 16:13:26 +0300

    finish dataBackup

--- utils/databackup/main.go
+++ utils/databackup/main.go
@@ -18,9 +18,11 @@ import (
 type WBDataBackup struct {
 	StName string
 	Data []weatherbitsvc.WBData
+
 }
 
 func main() {
+
 	configFile := flag.String("config.file", "config.yml", "Config file name.")
 	flag.Parse()
 
@@ -48,25 +50,12 @@ func main() {
 	source := impl.NewWeatherBitSVCClient(conf.Clients.SRCAddr, logger)
 
 	var wg  sync.WaitGroup
-	wg.Add(2)
+	wg.Add(1)
 
 	level.Info(logger).Log("msg", "Data backup client start")
 	defer level.Info(logger).Log("msg", "client end")
 
-	ch  := make(chan WBDataBackup)
-
-
-	go moveData(db, source, logger, ch, &wg)
-	go pushData(db,logger, ch, &wg)
-
-	wg.Wait()
-	level.Info(logger).Log("msg", "The program has finished")
-}
-
-func moveData(db *database.Postgres, source weatherbitsvc.Client, logger log.Logger, ch chan WBDataBackup, wg *sync.WaitGroup) {
-	//fmt.Print(ch)
 	level.Info(logger).Log("msg", "Getting data")
-	//currentTime := time.Now()
 	stations, err := source.GetStationsList()
 	if err != nil {
 		level.Error(logger).Log("msg", "Getting stations list error", "error", err.Error())
@@ -75,18 +64,32 @@ func moveData(db *database.Postgres, source weatherbitsvc.Client, logger log.Log
 
 	level.Info(logger).Log("msg", "Getting stations list success", "stations count", len(*stations))
 
+	ch  := make(chan WBDataBackup)
+
+	go moveData(db, source, logger, ch, *stations)
+	go pushData(db,logger, ch, &wg, len(*stations))
+
+	wg.Wait()
+	level.Info(logger).Log("msg", "The program has finished")
+}
+
+func moveData(db *database.Postgres, source weatherbitsvc.Client, logger log.Logger, ch chan WBDataBackup, stations []string) {
+
 	date := time.Now()
 	currentDate := date.Format(common.TimeLayoutWBH)
 
-	for i, stationName := range *stations {
+	for i, stationName := range stations {
 
 		date2 := time.Now()
 
 		level.Info(logger).Log("msg", "Process station: "+stationName, "#", i)
 		data, err := source.GetWBPeriod(stationName, "2000-01-01T00:00:00", currentDate)
 		if err != nil {
-
 			level.Error(logger).Log("msg", "Getting station data error", "station", stationName, "error", err.Error())
+			ch <- WBDataBackup{
+				stationName,
+				[]weatherbitsvc.WBData{},
+			}
 			continue
 		}
 
@@ -103,21 +106,27 @@ func moveData(db *database.Postgres, source weatherbitsvc.Client, logger log.Log
 	}
 }
 
-func pushData(db *database.Postgres, logger log.Logger, ch chan WBDataBackup, wg *sync.WaitGroup) {
-
-	for i := 0; i < 10000; i++ {
+func pushData(db *database.Postgres, logger log.Logger, ch chan WBDataBackup, wg *sync.WaitGroup, stations int) {
+	count := 0
+	for {
 		select {
 		case wd := <-ch:
+			count++
+
 			date := time.Now()
 			level.Info(logger).Log("msg", "Station: "+wd.StName+" data received")
+
 			err := db.PushWBData(wd.StName, wd.Data)
 			if err != nil {
 				level.Error(logger).Log("msg", "Saving station data error", "station", wd.StName, "error", err.Error())
 			}
 			fmt.Println("PushWBPeriod ", wd.StName, "complete. Time elapsed:", time.Since(date).Seconds(), "ms")
+			if count == stations {
+				wg.Done()
+			}
 		}
 	}
-	defer wg.Done()
+
 }
 
 
